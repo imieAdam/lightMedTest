7aadf6d6d4f91d8bf09fc05629ed4e9585811f1d
imieAdam <50846353+imieAdam@users.noreply.github.com>
2022-10-26 15:24:32 +0200

    config update

--- test/test_login_logout.py
+++ test/test_login_logout.py
@@ -3,14 +3,16 @@ import unittest
 from src.api_consumer import ApiConsumer
 
 class TestLoginLogout(unittest.TestCase):
-    def test_login_logout(self): 
+    def test_login_logout(self):
         test_apis = ApiConsumer()
         test_apis.read_config()
+        you_were_logged_out = test_apis.config["logged_out"]
         response = test_apis.login()
         assert len(response.history) == 2
         assert response.history[0].status_code == 302
         assert response.history[1].status_code == 302
         assert response.status_code == 200
+        assert (test_apis.config["logged_in"] in response.content.decode("utf-8"))
         response = test_apis.logout()
         assert response.status_code == 200
-        assert ('Zaloguj si' in str(response.content))
+        assert (you_were_logged_out in response.content.decode("utf-8"))
